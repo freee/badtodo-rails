9850644597c63df8895dc1a8b2aa3479584c469c
take32457 <100368610+take32457@users.noreply.github.com>
2023-09-13 14:28:40 +0900

    Update App.tsx

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Router from './router';
+import Router from './router/index';;
 
 function App() {
   return (
