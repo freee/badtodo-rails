483087aac50219db312ca1f5275336407fdd08b3
take32457 <issei-takenaka@freee.co.jp>
2023-09-14 17:18:52 +0900

    router周りのリファクタ

--- frontend/app/src/components/Navbar.tsx
+++ frontend/app/src/components/Navbar.tsx
@@ -16,31 +16,31 @@ type NavbarProps = {
         <li>
           <Link to="/">一覧</Link>
         </li>
-				<li>
-					<Link to="/new-todo">新規追加</Link>
-				</li>
-				<li>
-					<Link to="/contact">お問い合わせ</Link>
-				</li>
+		<li>
+			<Link to="/new-todo">新規追加</Link>
+		</li>
+		<li>
+			<Link to="/contact">お問い合わせ</Link>
+		</li>
         {!loggedIn && (
-					<li>
-						<Link to="/login">ログイン</Link>
-					</li>
+			<li>
+				<Link to="/login">ログイン</Link>
+			</li>
         )}
         {loggedIn && (
-					<>
-						<li>
-							<Link to="/mypage">マイページ</Link>
-						</li>
-						{isAdmin && (
-							<li>
-								<Link to="/member-management">会員管理</Link>
-							</li>
-						)}
-						<li>
-							<Link to="/logout">ログアウト</Link>
-						</li>
-					</>
+			<>
+				<li>
+					<Link to="/mypage">マイページ</Link>
+				</li>
+				{isAdmin && (
+					<li>
+						<Link to="/member-management">会員管理</Link>
+					</li>
+				)}
+				<li>
+					<Link to="/logout">ログアウト</Link>
+				</li>
+			</>
         )}
       </ul>
     </nav>
