c38fa110b55dfd95b06589f188cebb2944de22b3
take32457 <issei-takenaka@freee.co.jp>
2023-09-14 14:59:44 +0900

    画面遷移

--- frontend/app/src/router/index.tsx
+++ frontend/app/src/router/index.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import  Home  from '../views/Home';
 import Register from '../views/Register';
+import Inquery from '../views/Inquery';
+import NewToDo from '../views/NewToDo';
+import Profile from '../views/Profile';
+import ToDoList from '../views/ToDoList';
 
 function Router() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/register" element={<Register isAdmin={true}/>} />
+			<Route path="/contact" element={<Inquery />}/>
+			<Route path="/new-todo" element={<NewToDo />}/>
+			{/* <Route path="/mypage" element={<Profile/>}/> error出る*/}
+			
 		</Routes>
 	);
 }
