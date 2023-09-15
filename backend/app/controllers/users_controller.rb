4bad58cd3989844c9dd0e54b030a8d0b03deb1b5
lucvil <atsuki-yoshimura@freee.co.jp>
2023-09-15 19:12:11 +0900

    user showを作成

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
-	# GET /todos
+	before_action :set_user, only: %i[ show ]
+
+	# GET /users
 	def index
 		@users = User.all
 		
 		render json: @users
 	end
 
-end
+	# GET /users/1
+	def show
+		render json: @user
+	end
+
+	private
+	# Use callbacks to share common setup or constraints between actions.
+		def set_user
+			@user = User.find(params[:id])
+		end
+	end
+
