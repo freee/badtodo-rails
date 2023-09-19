8f11d649c0392badf2ba9c9ce72ac7d83427ab72
take32457 <issei-takenaka@freee.co.jp>
2023-09-19 12:29:48 +0900

    httpリクエストに付加するheader情報を取得する関数の作成

---
+++ frontend/app/src/utils/getHeaders.ts
@@ -0,0 +1,16 @@
+const addHeaders = () => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+        const token = JSON.parse(atob(storedToken));
+        console.log(token);
+        const params = { 
+            uid: `${token["uid"]}`,
+            client: `${token["client"]}`,
+            "access-token": `${token["access-token"]}`
+        }
+    
+    return params
+    }   
+}
+
+export default addHeaders;
